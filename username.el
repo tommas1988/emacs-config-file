bc5056ba4ebf902e94e07722728bb973b382de3a
Tommas Yuan <tommas1988@live.com>
2014-07-28 10:24:46 +0800

    change minibuffer-prompt

--- username.el
+++ username.el
@@ -67,7 +67,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Bitstream Vera Sans Mono" :foundry "outline" :slant italic :weight normal :height 113 :width normal))))))
+ '(default ((t (:family "Bitstream Vera Sans Mono" :foundry "outline" :slant italic :weight normal :height 113 :width normal)))))
+ ;; set minibuf font color
+(set-face-foreground 'minibuffer-prompt "white"))
 
 ;; initialize color-theme
 (load-theme 'monokai)
