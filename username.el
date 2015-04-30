ae358da32200ce914217edc2a5a9c78fd3efffc9
tommas1988 <tommas1988@live.com>
2015-04-30 16:10:07 +0800

    correct some comments and change the font type

--- username.el
+++ username.el
@@ -4,7 +4,7 @@
 ;; php-mode setting
 ;; set php executable for php-mode
 (set-variable 'php-executable "php-interpreter")
-;; set flymale-php
+;; set flymake-php
 (add-hook 'php-mode-hook 'flymake-php-load)
 ;; set hs-minor-mode
 (add-hook 'php-mode-hook 'hs-minor-mode)
@@ -77,7 +77,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Bitstream Vera Sans Mono" :foundry "outline" :slant italic :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Lucida Console" :foundry "outline"  :weight normal :height 113 :width normal)))))
  ;; set minibuf font color
 (set-face-foreground 'minibuffer-prompt "white"))
 
